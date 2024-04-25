41c39131ef71e826cc29c1b23975fa747c4aff6c
Paul Michael <paullmich28@gmail.com>
2024-04-25 11:15:04 +0700

    Initial Commit

---
+++ SuperHuman/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SuperHuman
+//
+//  Created by Paulus Michael on 25/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SuperHuman/SuperHumanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHumanApp.swift
+//  SuperHuman
+//
+//  Created by Paulus Michael on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SuperHumanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
